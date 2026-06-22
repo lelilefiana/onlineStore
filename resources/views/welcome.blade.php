297de52d3291a2d3040fd4fc4decead4362847cf
Leli Lefiana <lelilefiana0@gmail.com>
2026-06-22 14:17:34 +0800

    bootstrap ui version 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
-@section('title','Home Page - Online Store')
+@section('title', 'Home Page - Online Store')
 @section('content')
-<div class="text-center"> Welcome to the application</div>
+    <div class="text-center">
+        Welcome to the application
+    </div>
+
+    <div class="container mt-5">
+        <h1 class="text-primary">Online Store V1</h1>
+        <button class="btn btn-success">Belanja Sekarang</button>
+    </div>
 @endsection
